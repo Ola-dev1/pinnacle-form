55155ee89182282a9b02a743e822207e19351df9
adekunle olamilekan <adekunleolamilekan756@gmail.com>
2025-08-20 12:34:54 +0100

    Update backend CORS config

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ dotenv.config();
 const app = express();
 app.use(
     cors({
-        origin: ["http://localhost:3000", "http://localhost:5173"],
+        origin:
+            "https://pinnacle-form.onrender.com"
+        ,
         methods: ["GET", "POST"],
         allowedHeaders: ["Content-Type"],
     })
